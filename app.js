84245278b1d49148150a836389b36886d005608b
Dheeraj8623 <dheerajraje122@gmail.com>
2022-02-24 20:35:30 +0530

    model

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-<<<<<<< HEAD
-const db = require('./model/pg')
-=======
 
->>>>>>> 896d7a3e86d38216a4a770a35e3e94ee1e2c0d3c
+const db = require('./model/pg')
+
+
+
 var app = express();
 
 app.use(logger('dev'));
@@ -17,11 +17,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-<<<<<<< HEAD
 db.sequelize.sync();
 // db.sequelize.sync({force:true});
-=======
->>>>>>> 896d7a3e86d38216a4a770a35e3e94ee1e2c0d3c
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
